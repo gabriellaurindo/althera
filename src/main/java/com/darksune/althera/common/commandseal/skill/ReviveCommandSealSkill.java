c7a292f30376ddc95b5dd86c86b44defdeda9078
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2026-05-10 22:49:16 -0300

    todo

--- src/main/java/com/darksune/althera/common/commandseal/skill/ReviveCommandSealSkill.java
+++ src/main/java/com/darksune/althera/common/commandseal/skill/ReviveCommandSealSkill.java
@@ -12,7 +12,7 @@ public class ReviveCommandSealSkill extends AbstractCommandSealSkill {
     private static final int DURATION_TICKS = 20;
     private static final int COOLDOWN_TICKS = DURATION_TICKS + 20;
 
-    //todo criar niveis 1 nivel = 30 segundos e status +1, 2 nivel = 20s status +2, 3 nivel = 10s status + 3
+    //todo criar uma skill de Heal, gasta 50 de mana e recupera toda a vida do summon, se tiver vivo claro
     @Override
     public void execute(Player player, HeroEntity heroEntity) {
 
